e70a8d5d8265233717f144628222578b93d2eba6
Alexnorvag <norvag@mail.ru>
2018-06-29 10:20:21 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App/App';
+import registerServiceWorker from './Default/registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
